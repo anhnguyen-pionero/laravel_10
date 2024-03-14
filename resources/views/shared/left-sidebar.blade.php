b069a3479c315fcdbb20436a994b41b9000a2c8a
anh.nguyen-pionero <anh.nguyen@pionero.io>
2024-03-14 09:31:55 +0700

    feat: add style menu active

--- resources/views/shared/left-sidebar.blade.php
+++ resources/views/shared/left-sidebar.blade.php
@@ -2,30 +2,17 @@
      <div class="card-body pt-3">
          <ul class="nav nav-link-secondary flex-column fw-bold gap-2">
              <li class="nav-item">
-                 <a class="nav-link text-dark" href="#">
+                 <a class="nav-link text-dark {{ Route::is('home') ? 'text-white bg-primary rounded' : '' }}"
+                     href={{ route('home') }}>
                      <span>Home</span></a>
              </li>
+
              <li class="nav-item">
-                 <a class="nav-link" href="#">
-                     <span>Explore</span></a>
-             </li>
-             <li class="nav-item">
-                 <a class="nav-link" href="#">
-                     <span>Feed</span></a>
-             </li>
-             <li class="nav-item">
-                 <a class="{{ Route::is('terms') ? 'text-white bg-primary rounded' : '' }} nav-link"
-                     href="{{ route('terms')}}">
+                 <a class="nav-link text-dark {{ Route::is('terms') ? 'text-white bg-primary rounded' : '' }}"
+                     href="{{ route('terms') }}">
                      <span>Terms</span></a>
              </li>
-             <li class="nav-item">
-                 <a class="nav-link" href="#">
-                     <span>Support</span></a>
-             </li>
-             <li class="nav-item">
-                 <a class="nav-link" href="#">
-                     <span>Settings</span></a>
-             </li>
+
          </ul>
      </div>
      <div class="card-footer text-center py-2">
